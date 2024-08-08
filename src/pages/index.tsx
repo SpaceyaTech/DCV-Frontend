1534ed0c0e41407b05e81f664748012d60ab2d9f
Samoina <samoina.w@gmail.com>
2024-08-08 18:32:54 +0300

    Create Hero Card section for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Header from "@/components/Header";
+import Hero_Card from "@/components/Hero_Card_Section";
+
 export default function Index() {
   return (
     <>
       <Header />
+      <Hero_Card />
     </>
   );
 }
