3bf183903cb8d48056581766a5955cd8f3deda4a
felixNyalenda <felixasejunior@gmail.com>
2024-08-15 15:10:39 +0300

    replace list styles with svg

--- src/components/_meet-the-children/ChildrenCard.tsx
+++ src/components/_meet-the-children/ChildrenCard.tsx
@@ -18,18 +18,37 @@ function ChildrenCard({ image, list }: ChidrenCardProps) {
       <img src={image} alt="age 1-5" className="object-cover lg:mr-6" />
 
       <div className="flex flex-col space-y-6">
-        <ul className="flex list-disc flex-col gap-6 text-base font-normal">
+        <ul className="flex list-none flex-col gap-6 text-base font-normal">
           {list.map(({ id, title, description }) => (
-            <li className="marker-size" key={id}>
-              <b>{title}</b>
-              {description}
+            <li className="flex gap-3" key={id}>
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mt-1"
+              >
+                <rect
+                  y="0.515625"
+                  width="16"
+                  height="16"
+                  rx="8"
+                  fill="#FFA10C"
+                />
+              </svg>
+
+              <p className="flex-1">
+                <b>{title}</b>
+                {description}
+              </p>
             </li>
           ))}
         </ul>
 
         <Button
           size="default"
-          className="ms:self-start flex items-center gap-2 self-center rounded-full bg-primary-main text-base font-medium text-white"
+          className="flex items-center gap-2 self-center rounded-full bg-primary-main text-base font-medium text-white sm:self-start"
         >
           Support us today{" "}
           <img src={heartIcon} alt="heart" loading="lazy" className="w-4" />
