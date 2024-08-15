6cf8b6ee9ba1be2e4adc9acad6eb01128beb3f11
felixNyalenda <felixasejunior@gmail.com>
2024-08-15 12:40:27 +0300

    render staff data

--- src/components/MeetTheTeam/TeamCard.tsx
+++ src/components/MeetTheTeam/TeamCard.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+export interface StaffMember {
+  id?: number;
+  name: string;
+  role: string;
+  bg: string;
+  image: string;
+}
 
-import yvonne from "@/assets/images/meet-the-team/yvonne.jpeg";
-
-function TeamCard() {
+function TeamCard({ name, role, bg, image }: StaffMember) {
   return (
-    <div className="flex h-96 w-full max-w-72 flex-col overflow-hidden rounded-xl bg-primary-main">
+    <div
+      className="flex h-96 w-full max-w-72 flex-col overflow-hidden rounded-xl bg-primary-main"
+      style={{ backgroundColor: bg }}
+    >
       <div className="h-3/4">
-        <img src={yvonne} alt="" className="size-full object-cover" />
+        <img src={image} alt="" className="h-72 w-full object-cover" />
       </div>
-      <div className="mt-3.5 h-1/4 rounded-b-xl border border-primary-main bg-white">
+      <div
+        className="mt-3.5 h-1/4 rounded-b-xl border border-primary-main bg-white"
+        style={{ borderColor: bg }}
+      >
         <div className="flex flex-col p-2.5">
-          <h2 className="text-xl font-medium text-primary-1000">
-            Yvonne ter Avest
-          </h2>
+          <h2 className="text-xl font-medium text-primary-1000">{name}</h2>
 
-          <p className="text-sm italic text-gray-600">Co-Founder</p>
+          <p className="text-sm italic text-gray-600">{role}</p>
         </div>
       </div>
     </div>
--- src/pages/MeetTheTeam.tsx
+++ src/pages/MeetTheTeam.tsx
@@ -3,31 +3,48 @@ import React from "react";
 import DonateComponent from "@/components/Donate/DonateComponent";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header";
-import TeamCard from "@/components/MeetTheTeam/TeamCard";
+import TeamCard, { StaffMember } from "@/components/MeetTheTeam/TeamCard";
+
+import athman from "@/assets/images/meet-the-team/athman.jpeg";
+import chebet from "@/assets/images/meet-the-team/chebet.jpeg";
+import esther from "@/assets/images/meet-the-team/esther.jpeg";
+import faith from "@/assets/images/meet-the-team/faith.jpeg";
+import ishmael from "@/assets/images/meet-the-team/ishmael.jpeg";
+import maryMother from "@/assets/images/meet-the-team/mary-mother.jpeg";
+import maryNyanya from "@/assets/images/meet-the-team/mary-nyanya.jpeg";
+import mary from "@/assets/images/meet-the-team/mary.jpeg";
+import rose from "@/assets/images/meet-the-team/rose.jpeg";
+import yvonne from "@/assets/images/meet-the-team/yvonne.jpeg";
 
 function MeetTheTeam() {
   return (
     <main className="space-y-12">
       <Header />
-      <section className="container mx-auto flex max-w-[90%] flex-col items-center gap-10 border border-red-400">
-        <h1 className="text-xl font-bold sm:text-2xl lg:text-5xl">
-          Meet our Staff
-        </h1>
+      <section className="container mx-auto flex max-w-[90%] flex-col items-center gap-10 md:pb-6 lg:pb-12">
+        <h1 className="text-5xl font-bold">Meet our Staff</h1>
 
         <div className="flex w-full flex-col items-center gap-6 md:gap-10">
           <div className="flex w-full flex-col items-center justify-center gap-8 md:flex-row lg:gap-12">
-            <TeamCard />
-            <TeamCard />
+            {staff.slice(0, 2).map(({ id, name, role, bg, image }) => (
+              <TeamCard
+                key={id}
+                name={name}
+                role={role}
+                bg={bg}
+                image={image}
+              />
+            ))}
           </div>
-          <div className="grid w-full grid-cols-1 justify-items-center gap-8 border border-green-600 md:grid-cols-2 lg:grid-cols-4">
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
+          <div className="grid w-full grid-cols-1 justify-items-center gap-8 md:grid-cols-2 lg:grid-cols-4">
+            {staff.slice(2).map(({ id, name, role, bg, image }) => (
+              <TeamCard
+                key={id}
+                name={name}
+                role={role}
+                bg={bg}
+                image={image}
+              />
+            ))}
           </div>
         </div>
       </section>
@@ -39,11 +56,75 @@ function MeetTheTeam() {
 
 export default MeetTheTeam;
 
-const staff = [
+const staff: StaffMember[] = [
   {
     id: 1,
     name: "Yvonne ter Avest",
     role: "Co-Founder",
-    bg: "primary-main",
+    bg: "#0097F5",
+    image: yvonne,
+  },
+  {
+    id: 2,
+    name: "Esther Themba",
+    role: "Community Manager",
+    bg: "#FFA10C",
+    image: esther,
+  },
+  {
+    id: 3,
+    name: "Madam Faith",
+    role: "Social Worker",
+    bg: "#FFA10C",
+    image: faith,
+  },
+  {
+    id: 4,
+    name: "Madam Mary",
+    role: "House mother",
+    bg: "#003D63",
+    image: mary,
+  },
+  {
+    id: 5,
+    name: "Madam Rose",
+    role: "House mother",
+    bg: "#0097F5",
+    image: rose,
+  },
+  {
+    id: 6,
+    name: "Madam Mary",
+    role: "Nyanya",
+    bg: "#8C1823",
+    image: maryNyanya,
+  },
+  {
+    id: 7,
+    name: "Madam Chebet",
+    role: "House mother",
+    bg: "#0097F5",
+    image: chebet,
+  },
+  {
+    id: 8,
+    name: "Madam Mary",
+    role: "House mother",
+    bg: "#8C1823",
+    image: maryMother,
+  },
+  {
+    id: 9,
+    name: "Sir Athman",
+    role: "Driver",
+    bg: "#003D63",
+    image: athman,
+  },
+  {
+    id: 10,
+    name: "Sir Ishmael",
+    role: "Carpenter & Wood instructor",
+    bg: "#FFA10C",
+    image: ishmael,
   },
 ];
