948b0715b296afe3b7e9a3fb2d86f91f2350c430
felixNyalenda <felixasejunior@gmail.com>
2024-08-15 12:01:21 +0300

    create team card component

--- src/components/MeetTheTeam/TeamCard.tsx
+++ src/components/MeetTheTeam/TeamCard.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import yvonne from "@/assets/images/meet-the-team/yvonne.jpeg";
+
 function TeamCard() {
-  return <div className="border border-red-500">TeamCard</div>;
+  return (
+    <div className="flex h-96 w-full max-w-72 flex-col overflow-hidden rounded-xl bg-primary-main">
+      <div className="h-3/4">
+        <img src={yvonne} alt="" className="size-full object-cover" />
+      </div>
+      <div className="mt-3.5 h-1/4 rounded-b-xl border border-primary-main bg-white">
+        <div className="flex flex-col p-2.5">
+          <h2 className="text-xl font-medium text-primary-1000">
+            Yvonne ter Avest
+          </h2>
+
+          <p className="text-sm italic text-gray-600">Co-Founder</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TeamCard;
--- src/pages/MeetTheTeam.tsx
+++ src/pages/MeetTheTeam.tsx
@@ -9,17 +9,17 @@ function MeetTheTeam() {
   return (
     <main className="space-y-12">
       <Header />
-      <section className="container mx-auto flex max-w-screen-xl flex-col items-center gap-10 border border-red-400">
+      <section className="container mx-auto flex max-w-[90%] flex-col items-center gap-10 border border-red-400">
         <h1 className="text-xl font-bold sm:text-2xl lg:text-5xl">
           Meet our Staff
         </h1>
 
-        <div className="flex flex-col items-center gap-6 md:gap-10">
-          <div className="flex flex-col items-center gap-8 md:flex-row">
+        <div className="flex w-full flex-col items-center gap-6 md:gap-10">
+          <div className="flex w-full flex-col items-center justify-center gap-8 md:flex-row lg:gap-12">
             <TeamCard />
             <TeamCard />
           </div>
-          <div className="grid w-full grid-cols-1 gap-8 border border-green-600 md:grid-cols-2 lg:grid-cols-4">
+          <div className="grid w-full grid-cols-1 justify-items-center gap-8 border border-green-600 md:grid-cols-2 lg:grid-cols-4">
             <TeamCard />
             <TeamCard />
             <TeamCard />
@@ -38,3 +38,12 @@ function MeetTheTeam() {
 }
 
 export default MeetTheTeam;
+
+const staff = [
+  {
+    id: 1,
+    name: "Yvonne ter Avest",
+    role: "Co-Founder",
+    bg: "primary-main",
+  },
+];
