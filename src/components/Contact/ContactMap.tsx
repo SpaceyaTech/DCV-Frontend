efe63b8be473672e597701d0bf33fa0e7f15beee
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-08-20 15:42:25 +0300

    Embed Google maps to contact page

--- src/components/Contact/ContactMap.tsx
+++ src/components/Contact/ContactMap.tsx
@@ -3,11 +3,21 @@ import React from "react";
 const ContactMap = () => {
   return (
     <section>
-      <div className="bg-red-500">
-        <div className="mx-auto text-center">map section</div>
+      <div className="mt-4 md:mt-8">
+        <iframe
+          width={"1036"}
+          height={"380"}
+          loading="lazy"
+          // allowfullscreen
+          // referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJDcw7i5NPQBgRU-0ZZz3rmG8&key=AIzaSyDB2ZqfJDDyugo2Lefzt7JQtJoLX_M7KI0"
+          className="mx-auto max-w-md rounded-lg lg:max-w-6xl"
+        ></iframe>
       </div>
     </section>
   );
 };
 
 export default ContactMap;
+
+//
