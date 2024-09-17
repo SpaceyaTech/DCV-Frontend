c9c84e6cec236fa10aaba8a9422d58508c0570fb
McbullIan <mcbullian@gmail.com>
2024-09-17 12:21:28 +0300

    feat: fix image sizes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 import tsPaths from "vite-tsconfig-paths";
@@ -23,7 +22,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    ViteImageOptimizer({}),
     mkcert({
       source: "coding",
     }),
