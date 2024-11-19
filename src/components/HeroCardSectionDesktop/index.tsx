56672138761295730ab2fe4d255dd8dc14e8d620
felixNyalenda <felixasejunior@gmail.com>
2024-11-19 16:29:44 +0300

    remove images on hero section landing page

--- src/components/HeroCardSectionDesktop/index.tsx
+++ src/components/HeroCardSectionDesktop/index.tsx
@@ -1,40 +1,27 @@
-import DecorativeImageTop from "@/assets/Hero-Decorative-1.png";
-import DecorativeImageBottom from "@/assets/Hero-Decorative-2.png";
 import { Button } from "../ui/button";
 import { NavLink } from "react-router-dom";
 
 const index = () => {
   return (
-    <div className="relative bottom-32 mx-auto w-fit rounded-xl bg-white text-base">
-      <div className="flex flex-col items-center justify-center gap-y-4 text-center">
-        <img
-          className="rounded-t-xl"
-          src={DecorativeImageTop}
-          alt="Top decorative pattern desktop"
-        />
-        <p className="text-[18px] italic text-[#898384]">
-          {" "}
+    <div className="relative bottom-36 mx-auto flex w-fit max-w-6xl flex-col items-center justify-center gap-y-8 rounded-2xl bg-white px-36 py-16 text-center font-santoshi text-base">
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-lg italic leading-5 text-[#898384]">
           Because every child deserves an amazing childhood
         </p>
 
-        <p className="text-5xl font-bold text-[#006AAC]">
+        <h2 className="text-5xl font-bold text-[#006AAC]">
           Support Diani Children’s Village
-        </p>
-
-        <p className="text-[18px] leading-[27px] text-[#726C6C]">
-          Over the years, DCV has served as a sanctuary for over 100 children,
-          transforming lives <br></br> through dedicated care and support.
-        </p>
-
-        <Button className="h-[46px] w-[296px] self-center rounded-[67px] bg-[#0097F5] text-[20px] transition-all ease-linear hover:scale-105 hover:bg-[#0097F5]">
-          <NavLink to="donate">Support us today 💛</NavLink>
-        </Button>
-        <img
-          className="rounded-b-xl"
-          src={DecorativeImageBottom}
-          alt="Bottom decorative pattern desktop"
-        />
+        </h2>
       </div>
+
+      <p className="text-lg text-[#726C6C]">
+        Over the years, DCV has served as a sanctuary for over 100 children,
+        transforming lives <br></br> through dedicated care and support.
+      </p>
+
+      <Button className="h-[46px] w-[296px] self-center rounded-[67px] bg-[#0097F5] text-[20px] transition-all ease-linear hover:scale-105 hover:bg-[#0097F5]">
+        <NavLink to="donate">Support us today 💛</NavLink>
+      </Button>
     </div>
   );
 };
